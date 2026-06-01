dadb850fec38371ce143633d943a6ebe50d61913
JustAPerson <tllpinkhat@gmail.com>
2026-05-31 21:07:06 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,25 +1,3 @@
-if not game:IsLoaded() then
-    game.Loaded:Wait()
-end
-
-local Players = game:GetService("Players")
-
-local player = Players.LocalPlayer
-while not player do
-    task.wait()
-    player = Players.LocalPlayer
-end
-
-local playerGui = player:WaitForChild("PlayerGui")
-local mainGui = playerGui:WaitForChild("MainGui")
-
-local sideTasks = mainGui
-    :WaitForChild("MainFrame")
-    :WaitForChild("SideTasks")
-    :WaitForChild("Container")
-
-sideTasks:Destroy()
-
 queue_on_teleport([[
 if not game:IsLoaded() then
     game.Loaded:Wait()
