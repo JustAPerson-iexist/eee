2a08078dc4133a2885dbf422d99544868904d711
JustAPerson <tllpinkhat@gmail.com>
2026-05-31 21:08:14 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -21,3 +21,25 @@ local sideTasks = mainGui
 
 sideTasks:Destroy() 
 ]])
+wait(5)
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+local Players = game:GetService("Players")
+
+local player = Players.LocalPlayer
+while not player do
+    task.wait()
+    player = Players.LocalPlayer
+end
+
+local playerGui = player:WaitForChild("PlayerGui")
+local mainGui = playerGui:WaitForChild("MainGui")
+
+local sideTasks = mainGui
+    :WaitForChild("MainFrame")
+    :WaitForChild("SideTasks")
+    :WaitForChild("Container")
+
+sideTasks:Destroy() 
